0fd4e2be9a3d6c9d905baaac0892ad0b3821de62
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-03-04 00:38:03 +0100

    Patched some fly bypasses

--- src/main/java/FairPlay/detections/Fly.java
+++ src/main/java/FairPlay/detections/Fly.java
@@ -1,5 +1,3 @@
-// Decompiled with: CFR 0.152
-// Class Version: 8
 package FairPlay.detections;
 import FairPlay.data.Ban;
 import FairPlay.log.Logger;
@@ -12,17 +10,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Fly implements Listener {
-    @EventHandler(priority=EventPriority.HIGHEST)
-    public void move(PlayerMoveEvent e) {
+    @EventHandler (priority=EventPriority.HIGHEST)
+    public void move (PlayerMoveEvent e) {
         Player player = e.getPlayer();
         if (!player.getAllowFlight()) {
             if((e.getTo().getY() - e.getFrom().getY()) >= 0.7) {
-                for (int x = -10; x <= 10; ++x) {
+                for (int x = -5; x <= 5; ++x) {
                     for (int y = -255; y <= 255; ++y) {
-                        for (int z = -10; z <= 10; ++z) {
+                        for (int z = -5; z <= 5; ++z) {
                             Location blockLoc = e.getPlayer().getLocation().clone().add(x, y, z);
                             if (blockLoc.getBlock().getType() != Material.SLIME_BLOCK) continue;
-                            Logger.log(e.getPlayer().getDisplayName() + " failed Fly (had like ~5 blocks per sec speed; jumped on slime blocks; there is a block in 15 radius) VL 30% | Is authed: " + e.getPlayer().hasPermission("ac.execnpccmds"));
+                            Logger.log(e.getPlayer().getDisplayName() + " failed Fly (had 0.7 blocks per sec speed; jumped on slime blocks; there is a block in 5 radius) VL 30% | Is authed: " + e.getPlayer().hasPermission("ac.execnpccmds"));
                             return;
                         }
                     }
@@ -32,5 +30,4 @@ public class Fly implements Listener {
             }
         }
     }
-
 }
